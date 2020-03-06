b79a0c92ce2017fdaf1c9bb8923a969224cd0cc3
Maximilian Wagenbach <maximilian.wagenbach@native-instruments.de>
2020-03-06 12:03:24 +0100

    Sorted sentences semantically into a JSON file.

--- reapair/settings.py
+++ reapair/settings.py
@@ -19,5 +19,5 @@ from pathlib import Path
 
 BASE_PATH = Path(__file__).resolve().parent
 ASSETS_PATH = BASE_PATH / "assets"
-SENTENCES = {"de_DE": "sentences-de_DE.txt"}
+SENTENCES = {"de_DE": "sentences-de_DE.json"}
 DEFAULT_TEMPLATE = ASSETS_PATH / "template.j2.html"
